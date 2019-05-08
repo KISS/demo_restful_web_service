03c4a1e04046078b8e623dfdbef16f171aebbaa3
KISS <me@luisam.com>
2019-05-07 22:04:00 -0400

    Created json data, added required endpoints

--- app.js
+++ app.js
@@ -1,7 +1,39 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('hello world'));
+app.get('/animals', (req, res) => {
+  fs.readFile(__dirname + '/' + 'animals.json', 'utf-8', (err, data) => {
+    if (err)
+      throw err;
+    else
+      res.end(data);
+  });
+});
+
+app.get('/animals/:id', (req, res) => {
+  fs.readFile(__dirname + '/' + 'animals.json', 'utf-8', (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      const json = JSON.parse(data);
+      const animals = json.animals;
+      let result = undefined;
+
+      for (animal of animals) {
+        if (animal.id == req.params.id)
+          result = animal;
+      }
+
+      if (result)
+        res.end(JSON.stringify(result));
+      else
+        res.status('404').send('404: Not Found');
+    }
+  });
+});
+
+app.get('*', (req, res) => res.status('404').send('404: Not Found'));
 
 app.listen(3000, () => {
   console.log('REST API running on Port 3000');
